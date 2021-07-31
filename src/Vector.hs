68a6864a5d2c0a8388f8ae7890cb6f898de42d4a
Andrew McKnight <thedonk@gmail.com>
2021-07-31 16:07:30 -0700

    Made Vector a Random instance and added a chain model

--- src/Vector.hs
+++ src/Vector.hs
@@ -1,34 +1,47 @@
 module Vector
-( Vector
+( Vector (V)
+, coords
+, zeroV
 , lengthSq
 , randomV
 , randomVIn
 , (|*)
 ) where
 
-import System.Random
+import System.Random as R
 
-type Vector = (Float, Float)
+data Vector = V Float Float deriving Show
+
+instance Random Vector where
+  randomR (V x1 y1, V x2 y2) g = (V x y, g3)
+    where
+      (x, g2) = randomR (x1, x2) g
+      (y, g3) = randomR (y1, y2) g2
+  random g = (fromAngle theta, g2)
+    where
+      (theta, g2) = randomR (-pi, pi) g
+
+type Angle = Float
+
+fromAngle :: Angle -> Vector
+fromAngle a = V (cos a) (sin a)
+
+coords :: Vector -> (Float, Float)
+coords (V x y) = (x, y)
+
+zeroV :: Vector
+zeroV = V 0 0
 
 lengthSq :: Vector -> Float
-lengthSq (x, y) = x^2 + y^2
+lengthSq (V x y) = x^2 + y^2
 
 randomV :: StdGen -> Float -> (Vector, StdGen)
-randomV seed len = ((x, y), newSeed)
-  where
-    (theta, newSeed) = randomR (-pi, pi) seed
-    x = len * cos theta
-    y = len * sin theta
-
+randomV seed len = (len |* unit, newSeed)
+  where (unit, newSeed) = random seed
 
 randomVIn :: StdGen -> Float -> (Vector, StdGen)
-randomVIn thSeed maxLen = ((x, y), newSeed)
-  where
-    (theta, lenSeed) = randomR (-pi, pi) thSeed
-    (r, newSeed) = randomR (0, 1) lenSeed :: (Float, StdGen)
-    len = sqrt r * maxLen
-    x = len * cos theta
-    y = len * sin theta
+randomVIn seed maxLen = randomV vSeed $ maxLen * sqrt lenFactor
+  where (lenFactor, vSeed) = randomR (0.0, 1.0) seed
 
 (|*) :: Float -> Vector -> Vector
-(|*) scale (x, y) = (scale * x, scale * y)
+(|*) scale (V x y) = V (scale * x) (scale * y)
--- src/Vectors.hs
+++ src/Vectors.hs
@@ -3,6 +3,9 @@ module Vectors
 , (|+)
 , (|-)
 , (|.)
+, fromTo
+, distSq
+, dist
 ) where
 
 import Vector
@@ -10,10 +13,22 @@ import Vector
 type Vectors = (Vector, Vector)
 
 (|-) :: Vector -> Vector -> Vector
-(|-) (x1, y1) (x2, y2) = (x1-x2, y1-y2)
+(|-) (V x1 y1) (V x2 y2) = V (x1-x2) (y1-y2)
 
 (|+) :: Vector -> Vector -> Vector
-(|+) (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+(|+) (V x1 y1) (V x2 y2) = V (x1+x2) (y1+y2)
 
 (|.) :: Vector -> Vector -> Float
-(|.) (x1, y1) (x2, y2) = x1*x2 + y1*y2
+(|.) (V x1 y1) (V x2 y2) = x1*x2 + y1*y2
+
+fromTo :: Vector -> Vector -> Int -> [Vector]
+fromTo _ _ 0 = []
+fromTo v1 _ 1 = [v1]
+fromTo (V x1 y1) (V x2 y2) n = zipWith V (fromTo' x1 x2 n) (fromTo' y1 y2 n) 
+  where fromTo' a b n = [a, a + (b - a) / fromIntegral n .. b]
+
+distSq :: Vectors -> Float 
+distSq (v1, v2) = lengthSq $ v2 |- v1
+
+dist :: Vectors -> Float
+dist = sqrt . distSq
