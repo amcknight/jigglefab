f45ddb9fa5bc7ae2d222bf2aa1153a1325ed28a5
Andrew McKnight <thedonk@gmail.com>
2021-10-23 12:37:22 -0700

    Changed to just building tri because will build pies later

--- src/Geometry/Tiling.hs
+++ src/Geometry/Tiling.hs
@@ -2,6 +2,9 @@ module Geometry.Tiling
 ( Wedge(..)
 , tileVoronoi
 ) where
+
+import Data.List (sortBy)
+import Data.Maybe (catMaybes)
 import Geometry.Vector
 import Geometry.Voronoi
 import Geometry.Line
@@ -9,7 +12,6 @@ import qualified Data.Vector as V
 import Orb
 import Color
 import Chem
-import Data.List (sortBy)
 import Geometry.Angle
 import Pair
 import Debug.Trace
@@ -21,7 +23,7 @@ tileVoronoi :: Chem c => V.Vector (Orb c) -> [Edge] -> [Wedge]
 tileVoronoi = concatMap . toWedges
 
 toWedges :: Chem c => V.Vector (Orb c) -> Edge -> [Wedge]
-toWedges os (Edge s@(Seg p1 p2) is) = buildWedge p1 p2 cps o1 ++ buildWedge p1 p2 cps o2
+toWedges os (Edge s@(Seg p1 p2) is) = catMaybes [buildWedge p1 p2 cps o1, buildWedge p1 p2 cps o2]
   where
     cps = case crossPointsAtUnit (Line (p1 |- origOrbPos) (p2 |- origOrbPos)) of
       OneCross c -> OneCross (c |+ origOrbPos)
@@ -30,29 +32,15 @@ toWedges os (Edge s@(Seg p1 p2) is) = buildWedge p1 p2 cps o1 ++ buildWedge p1 p
     origOrbPos = orbPos o1
     (o1, o2) = pmap (os V.!) is
 
-buildWedge :: Chem c => Position -> Position -> CrossPoints -> Orb c -> [Wedge]
-buildWedge p1 p2 NoCross (Orb o ch) = [toPie o p1 p2 (chemColor ch)]
-buildWedge p1 p2 (OneCross _) (Orb o ch) = [toPie o p1 p2 (chemColor ch)]
-buildWedge p1 p2 (TwoCross c1 c2) (Orb o ch) = case (compare (distSq p1 o) 1, compare (distSq p2 o) 1) of
-  (GT, GT) -> -- Both outside but cross
-    let (s1, s2, s3) = sort4 p1 (p2,c1,c2)
-    in [toPie o p1 s1 c, Tri o s1 s2 c, toPie o s2 s3 c]
-  (GT, _) -> -- Left out right in. at
-    let (s1, s2, s3) = sort4 p1 (p2,c1,c2)
-    in [toPie o p1 s1 c, Tri o s1 s2 c]
-  (_, GT) -> -- Right out left in. ta
-    let (s1, s2, s3) = sort4 p2 (p1,c1,c2)
-    in [toPie o p2 s1 c, Tri o s1 s2 c]
-  (_, _) -> [Tri o p1 p2 c] -- Both in
-  where
-    c = chemColor ch
+buildWedge :: Chem c => Position -> Position -> CrossPoints -> Orb c -> Maybe Wedge
+buildWedge p1 p2 (TwoCross c1 c2) (Orb o ch) = Just $ case (compare (distSq p1 o) 1, compare (distSq p2 o) 1) of
+  (GT, GT) -> let (s1, s2, s3) = sort4 p1 (p2,c1,c2) in Tri o s1 s2 c -- Both outside but cross
+  (GT, _) -> let (s1, s2, s3) = sort4 p1 (p2,c1,c2) in Tri o s1 s2 c -- Left out right in
+  (_, GT) -> let (s1, s2, s3) = sort4 p2 (p1,c1,c2) in Tri o s1 s2 c -- Right out left in
+  (_, _) -> Tri o p1 p2 c -- Both in
+  where c = chemColor ch
 buildWedge _ _ AllCross _ = error "Impossible for a circle and line to share all points"
---     (p1, p2) = case turnDirection o x1 x2 of
---       Nothing -> error "Colinear wedge shouldn't happen??"
---       Just CounterClockwise -> (x1, x2)
---       Just Clockwise -> (x2, x1)
-TODO: ADD clockwise check
-
+buildWedge _ _ _ _ = Nothing
 
 sort4 :: Position -> (Position, Position, Position) -> (Position, Position, Position)
 sort4 out (a,b,c) = (d,e,f)
