9bbc8b8b80666fea007270c98947b3ae94d0b8cd
Andrew McKnight <thedonk@gmail.com>
2022-04-04 14:16:41 -0700

    Simplified and fixed overlay except for when going passed leaf node

--- src/Overlay.hs
+++ src/Overlay.hs
@@ -20,40 +20,38 @@ updateOverlay :: Con -> Position -> Overlay -> Overlay
 updateOverlay c mpos NoOverlay = NoOverlay
 updateOverlay c mpos o@(Overlay opos tk) = case dir of
   Nothing -> o
-  Just justDir -> case compare layer (length tk + 1) of
-    GT -> NoOverlay
-    EQ -> if minDir <= justDir && justDir <= maxDir 
-          then Overlay opos $ case expandTk c (minDir, maxDir) justDir tk of
-             Nothing -> error "Impossible case since this is covered by above conditions"
-             Just newTk -> newTk
-          else NoOverlay
-    LT -> if minDir <= justDir && maxDir >= justDir
-          then Overlay opos tkPrefix
-          else NoOverlay
+  Just justDir -> case compare layer $ length tk of
+    GT -> case expandTk c (overlayRange c tk) justDir tk of
+      Nothing -> NoOverlay
+      Just newTk -> Overlay opos newTk
+    EQ -> case expandTk c (overlayRange c (init tk)) justDir (init tk) of
+      Nothing -> NoOverlay
+      Just newTk -> Overlay opos newTk
+    LT -> Overlay opos $ init tk
   where
-    (minDir, maxDir) = overlayRange c tkPrefix (0,1)
-    tkPrefix = take layer tk
     layer = ceiling $ mag / overlayThinkness
     dir = direction v
     mag = magnitude v
     v = mpos |- opos
 
 expandTk :: Con -> P Turn -> Turn -> Token -> Maybe Token
-expandTk c (from, to) dir tk = if from <= dir && dir <= to
-  then Just $ tk ++ maybe [] (\ns -> [ns!!nameI]) mbNames
-  else Nothing
-  where
-    nameI = floor $ fromIntegral numNames * (dir - from) / (to - from)
-    numNames = maybe 0 length mbNames
-    mbNames =conNames <$> getCon c tk
-    x = getCon c tk
+expandTk c (from, to) dir tk = case getCon c tk of
+  Nothing -> error $ "Type Error in Token: " ++ show tk
+  Just con -> let
+    nameI = floor $ fromIntegral (length names) * (dir - from) / (to - from)
+    names = conNames con
+    in if from <= dir && dir <= to
+      then Just $ tk ++ (\ns -> [ns!!nameI]) names
+      else Nothing
 
-overlayRange :: Con -> Token -> P Turn -> P Turn
-overlayRange c [] r = r
-overlayRange c (n:ns) (from, to) = case con c n of
+overlayRange :: Con -> Token -> P Turn
+overlayRange c tk = overlayRange' c tk (0,1)
+overlayRange' :: Con -> Token -> P Turn -> P Turn
+overlayRange' c [] r = r
+overlayRange' c (n:ns) (from, to) = case con c n of
   Nothing -> error $ "Searching for non-existent chemical token: " ++ n
   Just (i, nextC) -> let
     step = (to - from) / fromIntegral (length (subcons c))
     newFrom = from + fromIntegral i * step
     newTo = newFrom + step
-    in overlayRange nextC ns (newFrom, newTo)
+    in overlayRange' nextC ns (newFrom, newTo)
