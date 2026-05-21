fe4b793cba0e391f3844340eaf8e8df9cf12e23e
Andrew McKnight <thedonk@gmail.com>
2026-05-20 19:06:36 -0700

    Task 1: scaffold cargo binary with deps

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod fab;
+pub mod chemistry;
+pub mod rng;
+pub mod ccd;
+pub mod collide;
+pub mod grid;
+pub mod sim;
+pub mod render;
+pub mod app;
+
+pub fn run() -> anyhow::Result<()> {
+    env_logger::init();
+    println!("jigglefab P1");
+    Ok(())
+}
